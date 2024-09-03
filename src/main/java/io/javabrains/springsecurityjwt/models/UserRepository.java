0de185f3c12572b0a4111127b621cefa215f238e
avnnet <augusto.vyhmeister@gmail.com>
2024-09-03 11:42:49 -0400

    inicio

---
+++ src/main/java/io/javabrains/springsecurityjwt/models/UserRepository.java
@@ -0,0 +1,8 @@
+package io.javabrains.springsecurityjwt.models;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    // Puedes definir métodos de consulta personalizados aquí, si los necesitas
+    User findByUsername(String username);
+}
